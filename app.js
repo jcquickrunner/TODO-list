2e8328671b66cd4ccd9dcf7442022a1fff463b63
Juan <juan@seasonmint.com>
2019-12-28 18:58:55 -0500

    add capability to ad list items

--- app.js
+++ app.js
@@ -2,51 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.set('view engine', 'ejs');
+let items = []  //an array and not a variable bevause by submiting you are re assighning the item variable with a new item
 
-app.get("/", function (req, res) {
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/", function (req, res) { //this again because this is triggered whenever home route is accessed
     
    let today = new Date();
-   let currentDay =today.getDay();
-   let day="";
-   switch (currentDay) {
-       case 0:
-           day="Sunday";
-           
-           break;
-           case 1:
-           day="Monday";
-           
-           break;
-           case 2:
-           day="Tuesday";
-           
-           break;
-           case 3:
-           day="Wednesday";
-           
-           break;
-           case 4:
-           day="Thursday";
-           
-           break;
-           case 5:
-           day="Friday";
-           
-           break;
-           case 6:
-           day="Saturday";
-           
-           break;
-   
-       default:
-           console.log("error:current day is equal to "+ currentDay);
-   }
-    res.render("list",{kindOfDay:day}) //list is the name of the ejs file i am using 
+   let options ={
+       weekday: "long",
+       day:"numeric",
+       month:"long"
+   };
+ 
+  var day = today.toLocaleDateString("en-US", options)
+    res.render("list",{kindOfDay:day, newListItems:items}) //list is the name of the ejs file i am using 
 });
 
 app.listen(3000, function () {
     console.log("server started on port 3000");
 });
-
+app.post("/",function(req,res){
+    let item = req.body.newItem;
+items.push(item);
+res.redirect("/"); //this triggers (see line 5)  basically this just refreshed the page to show the new list item
+console.log("submit went through")
+})
 //res.write can be used to send multpl pieces of data because res.send is about a catch all simlart to else in a function.
